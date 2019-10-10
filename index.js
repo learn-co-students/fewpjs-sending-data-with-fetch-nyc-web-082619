bdd73da88793c3965d621f50c4b90ab6aab700f5
github username <github email address>
2019-10-10 17:10:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function addDog(name, breed){
+    fetch(`http://localhost:3000/dogs/`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+            dogName: `${name}`,
+            dogBreed: `${breed}`
+        })
+    })
+};
+
+function submitData(name, email){
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+                name: `${name}`,
+                email: `${email}`
+            })
+        }
+
+    fetch(`http://localhost:3000/users/`, configObj)
+    .then(object => object.json())
+    .then(function(data){
+        document.body.append(`${data.id}`)
+    })
+    .catch(alert)
+};
